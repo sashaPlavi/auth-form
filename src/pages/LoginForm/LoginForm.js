e07e76e6342be78135bc6483a00a50d7fd9d6d48
SashaPlavi <sasha.bingulac@gmail.com>
2021-01-09 22:16:09 +0100

    login style

--- src/pages/LoginForm/LoginForm.js
+++ src/pages/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
+import './LoginForm.css';
 
 export default class LoginForm extends Component {
   state = {
@@ -17,23 +18,25 @@ export default class LoginForm extends Component {
   };
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <div className="login-form-container">
+        <form onSubmit={this.handleSubmit} className="login-form ">
           <h1>Login</h1>
 
           <input
+            className="input-item"
             placeholder="Email"
             value={this.state.email}
             onChange={(e) => this.setState({ email: e.target.value })}
           />
 
           <input
+            className="input-item"
             placeholder="Password"
             value={this.state.password}
             onChange={(e) => this.setState({ password: e.target.value })}
           />
 
-          <input type="submit" value="Submit" />
+          <input className="input-btn-item" type="submit" value="Submit" />
         </form>
       </div>
     );
